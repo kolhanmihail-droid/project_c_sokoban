41617fcd5b04d3b061ec12dce302348fd4e7b9d5
MEGAGEM <kolhan.mihail@gmail.com>
2026-05-24 22:01:41 +0200

    add new funk to undo

--- include/logika_gry.h
+++ include/logika_gry.h
@@ -1,20 +1,28 @@
 #ifndef LOGIKA_GRY_H
 #define LOGIKA_GRY_H
 
-// Struktura przechowujaca caly stan gry Sokoban
+// Wymog na 5.0 - Dynamiczna struktura danych (Lista jednokierunkowa)
+typedef struct MoveNode {
+    int dx;
+    int dy;
+    int pushed_box;        // 1 jesli popchnelismy skrzynie, 0 jesli nie
+    struct MoveNode *next; // Wskaznik na poprzedni ruch
+} MoveNode;
+
 typedef struct {
-    char **map;      // Podwojny wskaznik - dynamiczna tablica 2D na mape
-    int width;       // Szerokosc poziomu
-    int height;      // Wysokosc poziomu
-    int player_x;    // Pozycja gracza na osi X
-    int player_y;    // Pozycja gracza na osi Y
+    char **map;      
+    int width;       
+    int height;      
+    int player_x;    
+    int player_y;    
+    MoveNode *history; // Poczatek naszej listy z historia ruchow
 } GameState;
 
-// Deklaracje funkcji
 void initGame(GameState *state, int w, int h);
 void freeGame(GameState *state);
-
-// NOWE: Funkcja do poruszania graczem
 void movePlayer(GameState *state, int dx, int dy);
+
+// NOWE: Funkcja do cofania ruchu
+void undoMove(GameState *state);
 
 #endif
