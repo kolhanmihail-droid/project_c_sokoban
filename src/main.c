f767eca43358eac02ee34dbe656bad47b9f0dc38
MEGAGEM <kolhan.mihail@gmail.com>
2026-05-24 21:10:22 +0200

    add hi text for main

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Witaj w projekcie Sokoban!\n");
+    printf("Inicjalizacja modulow...\n");
+
+    // Tutaj w przyszlosci dodamy glowna petle gry
+
+    printf("Koniec programu.\n");
+    return 0;
+}
